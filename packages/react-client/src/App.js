b56aaeecbf122e224bb2bfe39de3328c98151867
playtime881 <lucas2@live.com.au>
2023-01-10 17:26:30 +1100

    Implemented basic API response

--- packages/react-client/src/App.js
+++ packages/react-client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { Navbar } from "./global-components";
@@ -6,6 +6,19 @@ import { NoPage, Songpage, Home } from "./pages";
 
 function App() {
 	let Page;
+
+	const [apiResponse, setapiResponse] = useState("");
+
+	function callAPI() {
+		fetch("http://localhost:9000/songpage")
+			.then((res) => res.text())
+			.then((res) => setapiResponse(res));
+	}
+
+	useEffect(() => {
+		callAPI();
+	});
+
 	switch (window.location.pathname) {
 		case "/":
 			Page = Home;
@@ -21,6 +34,7 @@ function App() {
 	return (
 		<>
 			<Navbar />
+			<p className="App-intro">{apiResponse}</p>
 			<Page />
 		</>
 	);
